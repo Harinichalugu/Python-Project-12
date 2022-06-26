4f20935f72af58339974aeb86458e60e9e191e73
Harinichalugu <86306059+Harinichalugu@users.noreply.github.com>
2022-06-26 18:25:52 +0530

    .py file1

---
+++ String Concatenation.py
@@ -0,0 +1,8 @@
+#String Concatenation
+word1=input("adj: ")
+word2=input("adj: ")
+word3=input("verb: ")
+
+sentence = f"I'M an {word1} Person!.I'M also a {word2} Worker. I can do {word3}!"
+
+print(sentence)
